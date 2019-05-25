0d97f4ea64c714bad93f0a1e51c8dc377f6e76e7
Etha <Etha@DESKTOP-EIRNLE0>
2019-05-25 11:00:06 +0900

    text node 생성 및 css 스타일 적용

--- JavaFX_Tutorial/src/application/Main.java
+++ JavaFX_Tutorial/src/application/Main.java
@@ -1,33 +1,36 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			
-			//stage > scene > container > node
-			
-			Pane pane = new Pane();//컨테이너
-			
-			Scene scene = new Scene(pane, 800, 500);//씬
-			
-			primaryStage.setScene(scene);//스테이지
-			
-			primaryStage.show();
-			primaryStage.setTitle("이현우 GUI");
-			primaryStage.setResizable(false);//스테이지 창 사이즈 변경 불가.
-			
-		} catch(Exception e) {
+
+			// stage > scene > container > node
+			Label text = new Label();
+			text.setText("Hello World!");
+			// css 사용가능.
+			text.setStyle("-fx-font-size: 30;-fx-text-fill: orange;-fx-background-color:yellow;-fx-border-color: red;");
+
+			StackPane pane = new StackPane();
+			pane.getChildren().add(text);
+
+			Scene scene = new Scene(pane, 500, 300);
+
+			stage.setScene(scene);
+			stage.setTitle("헬로 월드");
+			stage.show();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
