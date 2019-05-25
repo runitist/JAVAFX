9233cb42181c6b0f4554d37fbaeda68645da8628
Etha <Etha@DESKTOP-EIRNLE0>
2019-05-25 13:25:15 +0900

    컨테이너에 맞게 사이즈가 변경하는 반응형 노드

--- JavaFX_Tutorial/src/application/Main.java
+++ JavaFX_Tutorial/src/application/Main.java
@@ -1,42 +1,42 @@
 package application;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	StackPane pane = new StackPane();
 	GridPane grid = new GridPane();
-	Scene scene = new Scene(grid, 550, 400);
+	Scene scene = new Scene(pane, 100, 100);
 
 	@Override
 	public void start(Stage stage) {
 		try {
 
 			// stage > scene > container > node
+			Button button1 = new Button("Button1");
+			Button button2 = new Button("Button1");
+			button1.prefWidthProperty().bind(pane.widthProperty());;
+			button1.prefHeightProperty().bind(pane.heightProperty());;
+			button2.prefWidthProperty().bind(pane.widthProperty());;
+			button2.prefHeightProperty().bind(pane.heightProperty());;
+//			button2.setMinHeight(20);
+//			button2.setMinWidth(75);
+			button2.setMaxHeight(200);
+			button2.setMaxWidth(200);
+//			button2.setMinSize(minWidth, minHeight);
+			
+			grid.addRow(0, button1, button2);
+			
+			pane.getChildren().add(grid);
+			pane.setStyle("-fx-border-color: black;");
 
-			Label label1 = new Label("AA");
-			Label label2 = new Label("B");
-			Label label3 = new Label("C");
-			
-			label1.setStyle("-fx-border-color:black; -fx-font-size:30;");
-			label2.setStyle("-fx-border-color:black; -fx-font-size:30;");
-			label3.setStyle("-fx-border-color:black; -fx-font-size:30;");
-			
-			grid.add(label1, 0, 0, 2, 1);
-			grid.add(label2, 0, 1, 1, 1);
-			grid.add(label3, 1, 1, 1, 1);
-			
-			grid.setAlignment(Pos.CENTER);
-			
 			stage.setScene(scene);
 			stage.show();
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
