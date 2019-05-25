1ee37bf19ec614d688db685160475ef0c14dc392
Etha <Etha@DESKTOP-EIRNLE0>
2019-05-25 14:15:00 +0900

    앨범 프로그램

--- JavaFX_Tutorial/src/application/Main.java
+++ JavaFX_Tutorial/src/application/Main.java
@@ -1,12 +1,17 @@
 package application;
 
-import java.io.File;
 import java.io.FileInputStream;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -16,17 +21,51 @@ public class Main extends Application {
 	public void start(Stage stage) {
 		try {
 
-			FileInputStream ips = new FileInputStream(this.getClass().getResource("").getPath()+"benjamin.jpg");
-			Image image = new Image(ips);
 			StackPane pane = new StackPane();
 			Scene scene = new Scene(pane, 500, 500);
+			
+			Image image1 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/benjamin.jpg"));
+			Image image2 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/benjamin-suter-1618674-unsplash.jpg"));
+			Image image3 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/benjamin-suter-1618675-unsplash.jpg"));
+			Image image4 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/ethan-dow-1617301-unsplash.jpg"));
+			Image image5 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/jonathan-borba-1618090-unsplash.jpg"));
+			Image def = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/rawkkim-1612965-unsplash.jpg"));
+			
 			ImageView iv = new ImageView();
-			iv.setImage(image);
-			iv.setRotate(-90);
 			iv.setFitWidth(pane.getWidth());
 			iv.setPreserveRatio(true);
 			
+			Label label1 = new Label(" 1 ");
+			Label label2 = new Label(" 2 ");
+			Label label3 = new Label(" 3 ");
+			Label label4 = new Label(" 4 ");
+			Label label5 = new Label(" 5 ");
+			
+			GridPane grid = new GridPane();
+			grid.addRow(0, label1, label2, label3, label4, label5);
+			grid.setAlignment(Pos.BOTTOM_CENTER);
+			
+			Map<Label, Image> map = new LinkedHashMap<Label, Image>();
+			map.put(label1, image1);
+			map.put(label2, image2);
+			map.put(label3, image3);
+			map.put(label4, image4);
+			map.put(label5, image5);
+			
+			iv.setImage(def);
+			
 			pane.getChildren().add(iv);
+			pane.getChildren().add(grid);
+			
+			for (Label label : map.keySet()) {
+				label.setStyle("-fx-border-color: black; -fx-font-size: 20;");
+				label.setOnMouseEntered(e->{
+					iv.setImage(map.get(label));
+				});
+				label.setOnMouseExited(e->{
+					iv.setImage(def);
+				});
+			}
 			
 			stage.setScene(scene);
 			stage.show();
