2dd9d3916a615d79296f4ef89ec8ebc1b8ba8e92
Etha <Etha@DESKTOP-EIRNLE0>
2019-05-25 12:42:55 +0900

    바둑판 정렬 학습코드1

--- JavaFX_Tutorial/src/application/Main.java
+++ JavaFX_Tutorial/src/application/Main.java
@@ -5,34 +5,32 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	GridPane grid = new GridPane();
+	Scene scene = new Scene(grid, 400, 400);
+	
 	@Override
 	public void start(Stage stage) {
 		try {
 
 			// stage > scene > container > node
-			Label label = new Label();
-			label.setText("0");
-			label.setStyle("-fx-font-size: 40");
-			
-			Button button = new Button();
-			button.setText("Add");
-			button.setStyle("-fx-font-size: 20");
-			button.setOnAction(event->{
-				label.setText((Integer.parseInt(label.getText())+1)+"");
-			});//람다식 사용. 메소드가 하나뿐인 인터페이스를 구현할때 마치 메소드 하나만 구현하고 넘겨주는 효과. 익명 클래스 생성
-			
-			
-			StackPane pane = new StackPane();
-			pane.getChildren().addAll(label, button);
-			Scene scene = new Scene(pane, 200, 200);
-			StackPane.setAlignment(button, Pos.BOTTOM_CENTER);//모든 stackpane 내부 노드를 정렬, Pos 객체의 상수를 이용하여 화면 좌표계 설정.
+			Label label1 = new Label("Hello");
+			Label label2 = new Label("World");
 
+			grid.setAlignment(Pos.CENTER);
+			grid.setHgap(10);
+			grid.setVgap(10);
+			
+			grid.addColumn(0, label1, label2);
+//			grid.addRow(1,label1, label2);
+			
 			stage.setScene(scene);
-			stage.setTitle("카운터기");
+			stage.setTitle("바둑판 정렬");
 			stage.show();
 
 		} catch (Exception e) {
