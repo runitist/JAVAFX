844363d017f80be98904cc064db8ea298d2bc116
Etha <Etha@DESKTOP-EIRNLE0>
2019-05-25 15:03:58 +0900

    그림판 완료

--- JavaFX_Tutorial/src/application/Main.java
+++ JavaFX_Tutorial/src/application/Main.java
@@ -1,73 +1,68 @@
 package application;
 
-import java.io.FileInputStream;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	Canvas canvas = new Canvas(800, 500);
+	GraphicsContext gc;
+	ColorPicker cp = new ColorPicker();
+	Slider slider = new Slider();
+	Label label = new Label();
+	GridPane grid = new GridPane();
+
+	StackPane pane = new StackPane();
+	Scene scene = new Scene(pane, 800, 500);
+
 	@Override
 	public void start(Stage stage) {
 		try {
+			gc = canvas.getGraphicsContext2D();
+			gc.setLineWidth(1);
 
-			StackPane pane = new StackPane();
-			Scene scene = new Scene(pane, 500, 500);
+			pane.getChildren().addAll(canvas, grid);
+			grid.addRow(0, cp, slider, label);
 			
-			Image image1 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/benjamin.jpg"));
-			Image image2 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/benjamin-suter-1618674-unsplash.jpg"));
-			Image image3 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/benjamin-suter-1618675-unsplash.jpg"));
-			Image image4 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/ethan-dow-1617301-unsplash.jpg"));
-			Image image5 = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/jonathan-borba-1618090-unsplash.jpg"));
-			Image def = new Image(new FileInputStream(this.getClass().getResource("").getPath()+"../../image/rawkkim-1612965-unsplash.jpg"));
+			slider.setMin(1);
+			slider.setMax(100);
+			slider.setShowTickLabels(true);
+			slider.setShowTickMarks(true);
+			slider.valueProperty().addListener(e->{
+				double value = slider.getValue();
+				String str = String.format("%.1f", value);
+				label.setText(str);
+				gc.setLineWidth(value);
+			});
 			
-			ImageView iv = new ImageView();
-			iv.setFitWidth(pane.getWidth());
-			iv.setPreserveRatio(true);
 			
-			Label label1 = new Label(" 1 ");
-			Label label2 = new Label(" 2 ");
-			Label label3 = new Label(" 3 ");
-			Label label4 = new Label(" 4 ");
-			Label label5 = new Label(" 5 ");
-			
-			GridPane grid = new GridPane();
-			grid.addRow(0, label1, label2, label3, label4, label5);
-			grid.setAlignment(Pos.BOTTOM_CENTER);
-			
-			Map<Label, Image> map = new LinkedHashMap<Label, Image>();
-			map.put(label1, image1);
-			map.put(label2, image2);
-			map.put(label3, image3);
-			map.put(label4, image4);
-			map.put(label5, image5);
-			
-			iv.setImage(def);
-			
-			pane.getChildren().add(iv);
-			pane.getChildren().add(grid);
-			
-			for (Label label : map.keySet()) {
-				label.setStyle("-fx-border-color: black; -fx-font-size: 20;");
-				label.setOnMouseEntered(e->{
-					iv.setImage(map.get(label));
-				});
-				label.setOnMouseExited(e->{
-					iv.setImage(def);
-				});
-			}
-			
+			cp.setValue(Color.BLACK);
+			cp.setOnAction(e->{
+				gc.setStroke(cp.getValue());
+			});
+
+			scene.setOnMousePressed(e -> {
+				gc.beginPath();
+				gc.lineTo(e.getSceneX(), e.getSceneY());
+				gc.stroke();
+			});
+
+			scene.setOnMouseDragged(e -> {
+				gc.lineTo(e.getSceneX(), e.getSceneY());
+				gc.stroke();
+			});
+
 			stage.setScene(scene);
-			stage.setTitle("¾Ù¹ü");
+			stage.setTitle("±×¸²ÆÇ");
 			stage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
