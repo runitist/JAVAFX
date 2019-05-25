089c71f4bb7f0adcaaaa505e857b1d21adf7bcc6
Etha <Etha@DESKTOP-EIRNLE0>
2019-05-25 13:51:33 +0900

    Image 객체 사용

--- JavaFX_Tutorial/src/application/Main.java
+++ JavaFX_Tutorial/src/application/Main.java
@@ -1,40 +1,33 @@
 package application;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	StackPane pane = new StackPane();
-	GridPane grid = new GridPane();
-	Scene scene = new Scene(pane, 100, 100);
-
 	@Override
 	public void start(Stage stage) {
 		try {
 
-			// stage > scene > container > node
-			Button button1 = new Button("Button1");
-			Button button2 = new Button("Button1");
-			button1.prefWidthProperty().bind(pane.widthProperty());;
-			button1.prefHeightProperty().bind(pane.heightProperty());;
-			button2.prefWidthProperty().bind(pane.widthProperty());;
-			button2.prefHeightProperty().bind(pane.heightProperty());;
-//			button2.setMinHeight(20);
-//			button2.setMinWidth(75);
-			button2.setMaxHeight(200);
-			button2.setMaxWidth(200);
-//			button2.setMinSize(minWidth, minHeight);
+			FileInputStream ips = new FileInputStream(this.getClass().getResource("").getPath()+"benjamin.jpg");
+			Image image = new Image(ips);
+			StackPane pane = new StackPane();
+			Scene scene = new Scene(pane, 500, 500);
+			ImageView iv = new ImageView();
+			iv.setImage(image);
+			iv.setRotate(-90);
+			iv.setFitWidth(pane.getWidth());
+			iv.setPreserveRatio(true);
 			
-			grid.addRow(0, button1, button2);
+			pane.getChildren().add(iv);
 			
-			pane.getChildren().add(grid);
-			pane.setStyle("-fx-border-color: black;");
-
 			stage.setScene(scene);
 			stage.show();
 		} catch (Exception e) {
